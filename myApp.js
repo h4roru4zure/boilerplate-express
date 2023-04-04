a70e2c85c9659c9b711faffb1e605b2b8a6847c6
h4roru <h4roru4zure@outlook.com>
2023-04-04 20:06:09 -0300

    absolute Path

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
 app.get("/",function(req, res) {
     res.send("Hello Express");
 });
 
+app.get("/",(req,res)=>{
+    absolutePath=__dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
 
 
 
