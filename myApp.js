37761c7466ddb2ad0f6afbc50348f64729f140cc
h4roru <h4roru4zure@outlook.com>
2023-04-04 20:42:42 -0300

    express.static

--- myApp.js
+++ myApp.js
@@ -1,16 +1,13 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/",function(req, res) {
-    res.send("Hello Express");
-});
 
 app.get("/",(req,res)=>{
-    absolutePath=__dirname + '/views/index.html';
+    let absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
 });
-
-
+let absolutePath = __dirname + "/public";
+app.use("/public",express.static(absolutePath));
 
 
 
